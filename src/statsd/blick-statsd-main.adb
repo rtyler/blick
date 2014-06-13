14ae87ef9991d4773341332d57b988daf3bbdfcd
R. Tyler Croy <tyler@monkeypox.org>
2014-06-13 11:46:33 -0700

    Restructure blick-statsd to use a task for metric composition from datagrams

--- src/statsd/blick-statsd-main.adb
+++ src/statsd/blick-statsd-main.adb
@@ -18,27 +18,58 @@ procedure Blick.Statsd.Main is
        Address_Type => Anet.Sockets.Inet.UDPv4_Sockaddr_Type,
        Receive     => Anet.Sockets.Inet.Receive);
 
-   procedure Process_Datagram (Item : Ada.Streams.Stream_Element_Array;
-                               Src  : Anet.Sockets.Inet.UDPv4_Sockaddr_Type) is
-      Buffer : Unbound.Unbounded_String;
+   ---
+   -- Simple utility funciton to turn a Ada.Streams.Stream_Element into a
+   -- single Character
+   ---
+   function To_Character (Byte : in Ada.Streams.Stream_Element) return Character is
+   begin
+      return Character'Val (Byte);
+   end To_Character;
 
-      function To_Character (Byte : in Ada.Streams.Stream_Element) return Character is
-      begin
-         return Character'Val (Byte);
-      end To_Character;
+
+   ---
+   -- The compositor task is responsible for collecting metrics as they come in
+   -- as raw datagrams and then flushing the results as they are composed
+   -- together
+   --
+   -- This separate from the datagram receive code to ensure that we can handle
+   -- metric data across datagram boundaries.
+   ---
+   task Compositor is
+      entry Process_Datagram (Item : Ada.Streams.Stream_Element_Array);
+   end Compositor;
+
+   task body Compositor is
+      Buffer : Unbound.Unbounded_String;
 
       use Ada.Streams;
    begin
-      Put_Line ("Process_Datagram");
-      for Byte : Ada.Streams.Stream_Element of Item loop
-         -- Skip the new lines
-         if Byte /= 16#a# then
-            Unbound.Append (Buffer, To_Character (Byte));
-         end if;
+      loop
+         accept Process_Datagram (Item : Ada.Streams.Stream_Element_Array) do
+            for Byte : Ada.Streams.Stream_Element of Item loop
+               -- Skip the new lines
+               if Byte /= 16#a# then
+                  Unbound.Append (Buffer, To_Character (Byte));
+               else
+                  Put_Line ("Metric received (" & Unbound.To_String (Buffer) & ") need to flush");
+                  Unbound.Delete (Buffer, 1, Unbound.Length (Buffer));
+               end if;
+            end loop;
+         end;
       end loop;
-      Put_Line ("Buffer => " & Unbound.To_String (Buffer));
-      Put_Line ("Exit Process_Datagram");
-   end Process_Datagram;
+   end Compositor;
+
+   ---
+   -- Receive the datagram straight off the socket and then send it to the
+   -- compositor. The compositor will handle parsing/processing the actual
+   -- datagram itself
+   ---
+   procedure Receive_Datagram (Item : Ada.Streams.Stream_Element_Array;
+                               Src  : Anet.Sockets.Inet.UDPv4_Sockaddr_Type) is
+   begin
+      Compositor.Process_Datagram (Item);
+   end Receive_Datagram;
 
    Socket   : aliased Anet.Sockets.Inet.UDPv4_Socket_Type;
    Receiver : UDP_Receiver.Receiver_Type (S => Socket'Access);
@@ -46,6 +77,6 @@ begin
    Put_Line ("Starting Blick Statsd relay");
 
    Socket.Init;
-   Socket.Bind (Port => 9053);
-   Receiver.Listen (Callback => Process_Datagram'Access);
+   Socket.Bind (Port => 8125);
+   Receiver.Listen (Callback => Receive_Datagram'Access);
 end Blick.Statsd.Main;
